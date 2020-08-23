782d9514666c75326554c10d895ebe695d75c163
thepi <thepigs@gmail.com>
2020-08-23 12:01:49 +1000

    Initial commit

---
+++ index.js
@@ -0,0 +1,75 @@
+// Import the appropriate service
+const {smarthome} = require('actions-on-google')
+const {Deferred} = require('./utils')
+const  {get_current_state,execute_commands} = require('./mqtt')
+
+// Create an app instance
+const app = smarthome()
+
+// Register handlers for Smart Home intents
+
+app.onExecute((body, headers) => {
+    console.log(body)
+    for (let input of body.inputs){
+        execute_commands(input.payload.commands)
+    }
+    return null
+})
+
+function makePayload(request,payload){
+
+}
+app.onQuery((body, headers) => {
+    let d = get_current_state().then(value=>{
+        let r = { requestId: body.requestId,
+            payload: value,
+            status: 'SUCCESS'
+        }
+        return r
+    })
+    return d
+})
+
+app.onSync((body, headers) => {
+    return {
+        requestId: body.requestId,
+        payload: {
+            agentUserId: "1836.15267389",
+            devices: [{
+                id: "456",
+                type: "action.devices.types.LIGHT",
+                traits: [
+                    "action.devices.traits.OnOff",
+                    "action.devices.traits.Brightness",
+                    "action.devices.traits.ColorSetting"
+                ],
+                name: {
+                    //     defaultNames: ["lights out inc. bulb A19 color hyperglow"],
+                    name: "Kitchen Light Bar",
+                    //    nicknames: ["reading lamp"]
+                },
+                willReportState: true,
+                roomHint: "Kitchen",
+                attributes: {
+                    colorModel: 'rgb',
+                },
+                deviceInfo: {
+                    manufacturer: "gina",
+                    model: "0",
+                    hwVersion: "0",
+                    swVersion: "0"
+                },
+            }
+            ]
+        }
+    };
+})
+
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const expressApp = express().use(bodyParser.json())
+
+expressApp.post('/fulfillment', app)
+
+expressApp.listen(5000)
---
+++ mqtt.js
@@ -0,0 +1,121 @@
+var mqtt = require('mqtt')
+var client = mqtt.connect('mqtt://192.168.0.128')
+var {Deferred} = require('./utils')
+const TOPIC = 'zigbee2mqtt/kitchen_light_bar'
+//
+// return {
+//     requestId: body.requestId,
+//     payload: {
+//         devices: {
+//             123: {
+//                 online: true,
+//                 color: {
+//                     spectrumRgb: 16711935
+//                 },
+//                 status: 'SUCCESS'
+//             }
+//         }
+//     }
+// };
+
+let cs_requests = []
+
+function get_current_state(resolve) {
+    let d = new Promise(resolve => {
+        cs_requests.push(resolve)
+        client.publish(TOPIC + '/get', '')
+    })
+    return d
+}
+
+function execute_commands(commands) {
+    let payload = {}
+    for (let command of commands) {
+        for (let execution of command.execution) {
+            let cmd = execution.command
+            console.log(cmd)
+
+            switch (cmd) {
+                case 'action.devices.commands.BrightnessAbsolute':
+                    payload['brightness'] = execution.params.brightness
+                    break
+                case 'action.devices.commands.ColorAbsolute':
+                    payload['color'] = rgb2xy(execution.params.color.spectrumRGB)
+                    break
+                case 'action.devices.commands.OnOff':
+                    payload['state'] = execution.params.on ? 'ON' : 'OFF'
+                    break
+            }
+        }
+    }
+    client.publish(TOPIC + '/set', JSON.stringify(payload))
+
+}
+
+client.on('connect', function () {
+    client.subscribe(TOPIC, function (err) {
+        if (!err) {
+            client.publish('googlehome', 'Hello mqtt')
+        }
+    })
+})
+
+//{"state":"OFF","linkquality":0,"brightness":0,"color_temp":203,"color":{"x":0.3565,"y":0.3391,"h":90,"s":71.42857142857142}}
+// devices: {
+//     123: {
+//         online: true,
+//             color: {
+//             spectrumRgb: 16711935
+//         },
+//         status: 'SUCCESS'
+//     }
+// }
+
+function rgb2xy(rgb){
+    let blue = rgb & 255
+    let green = (rgb>>8) & 255
+    let red = (rgb>>16) & 255
+    let X = red * 0.649926 + green * 0.103455 + blue * 0.197109;
+    let Y = red * 0.234327 + green * 0.743075 + blue * 0.022598;
+    let Z = red * 0.0000000 + green * 0.053077 + blue * 1.035763;
+
+    let x = X / (X + Y + Z);
+
+    let y = Y / (X + Y + Z);
+    return {x:x,y:y}
+}
+
+function to_google(msg) {
+
+    return {
+        devices: {
+            online: true,
+            on: msg.state === 'ON',
+            brightness: msg.brightness,
+        //     color: {
+        //         spectrumRGB: {
+        //             hue: msg.color.h,
+        //             saturation: msg.color.s,
+        //             value: msg.color.v
+        //         }
+        //     }
+        },
+    }
+}
+
+client.on('message', function (topic, message) {
+    // message is Buffer
+    let msg = message.toString()
+    if (topic === TOPIC) {
+        let cs = to_google(JSON.parse(msg))
+        let send = cs_requests
+        cs_requests = []
+        for (let d of send)
+            d(cs)
+    }
+
+})
+
+exports.get_current_state = get_current_state
+exports.execute_commands = execute_commands
+
